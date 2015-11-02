188cf20558044fc9fcfde585e0da01dfa89e2353
Palladin <Participant@pochta.ru>
2015-11-02 09:12:20 +0200

    not allrite code, only for can read from another plase

--- button.c
+++ button.c
@@ -1,95 +1,84 @@
 #include  "button.h"
 
-typedef  enun {
-    RELEASED,
-	PRESSED,
-#if  (BUTTON_LONG_PRESSED == 1)
-    LONG_PRESSED,
-#endif
-//#if  (USE_LONG_RELEASED == 1)
-//    LONG_RELEASED,
-//#endif
-} BUTTON_STATE_T;
+
+extern void ButtonsCallback(BASE_T id);
+
+extern BUTTON_T *Buttons[];
 
 
-typedef  struct {
-    BASE_T Counter_Ms;
-    BUTTON_CONDITION_T Condition_Current;
-    const BASE_T *Counter_Mark_Ms;    	
-#if  (BUTTON_USE_LONG_PRESSED == 1)
-    const BASE_T *Counter_Long_Mark_Ms;
-#endif
-//    BUTTON_CONDITION_T Condition_Previous;
-	BOOL (* Pin_Chack) (void);
-	void (* Pin_Init) (void);
-} BUTTON_T;
-
-extern enuum ButtonsId;
-BUTTON_T *Buttons[ButtonsId];
-
-
-#if  (BUTTON_USE_LONG_PRESSED == 1)
-    const BASE_T *Counter_Long_Mark_Ms;
-void ButtonsCreate (ButtonsId id, const BASE_T *counter_mark_ms, const BASE_T *counter_long_mark_ms, BOOL (* pin_chack) (void), void (* pin_init) (void)) 
+void ButtonsCreate (BASE_T id, const BASE_T *counter_mark_ms, const BASE_T *counter_long_mark_ms, BOOL (* pin_chack) (void), void (* pin_init) (void)) 
 {
-    Buttons[id].Counter_Ms = 0;
-	Buttons[id].Condition_Current = RELEASED;
-	Buttons[id].Counter_Mark_Ms = counter_mark_ms;
-    Buttons[id].Counter_Long_Mark_Ms = counter_long_mark_ms;
-    Buttons[id].Pin_Chack = pin_chack;
-	Buttons[id]->Pin_Init = pin_init; 
+    Buttons[id]->Counter_Ms = 0;
+	Buttons[id]->Condition_Current = RELEASED;
+	Buttons[id]->Counter_Mark_Ms = counter_mark_ms;
+
+	#if  (BUTTON_USE_LONG_PRESSED == 1)
+    Buttons[id]->Counter_Long_Mark_Ms = counter_long_mark_ms;
+	#endif
+
+    Buttons[id]->Pin_Chack = pin_chack;
+    Buttons[id]->Pin_Init = pin_init; 
 }	
-#else
-void ButtonsCreate (ButtonsId id, const BASE_T *counter_mark_ms, BOOL (* pin_chack) (void), void (* pin_init) (void)) 
-{
-    Buttons[id].Counter_Ms = 0;
-	Buttons[id].Condition_Current = RELEASED;
-	Buttons[id].Counter_Mark_Ms = counter_mark_ms;
-    Buttons[id].Pin_Chack = pin_chack;
-	Buttons[id]->Pin_Init = pin_init;  
-}
-#endif
 
 
 void ButtonsInit (void) 
 {
-    u08 buttons_number;
+    u8 buttons_number;
 
-    for (buttons_number = 0; buttons_number < sizeof(Buttons[ButtonsId]) / sizeof(Buttons[0]); buttons_number++) {
-	    Buttons[buttons_number]->Pin_Init; 
+    for (buttons_number = 0; buttons_number < sizeof(Buttons[2]) / sizeof(Buttons[0]); buttons_number++) {
+//	    if (Buttons[buttons_number].Pin_Init != 0) {
+		   Buttons[buttons_number]->Pin_Init();
+//		} 
 	}	
+
+/*
+    while (*Buttons++ != NULL) {
+	    *Buttons->Pin_Init;
+	}
+*/
 }
 
 
 void ButtonsPolling (BASE_T timer_period_ms)
 {
-    u08 buttons_number;
+    u8 buttons_number;
 
-    for (buttons_number = 0; buttons_number < sizeof(Buttons[ButtonsId]) / sizeof(Buttons[0]); buttons_number++) {
-	    if (Buttons[buttons_number]->Pin_Chack == 1) {
-		    if (Buttons[buttons_number].Counter >= Buttons[buttons_number].Counter_Mark) {
-			    if (Buttons[buttons_number].Condition_Current != PRESSED) {
-				    Buttons[buttons_number].Condition_Current = PRESSED;
+    for (buttons_number = 0; buttons_number < sizeof(Buttons[2]) / sizeof(Buttons[0]); buttons_number++) {
+	    if (Buttons[buttons_number]->Pin_Chack() == 1) {
+		    if (Buttons[buttons_number]->Counter_Ms >= Buttons[buttons_number]->Counter_Mark_Ms) {
+			    if (Buttons[buttons_number]->Condition_Current != PRESSED) {
+				    Buttons[buttons_number]->Condition_Current = PRESSED;
+					
+					#if  (BUTTON_CALLBACK == 1) 
+					ButtonsCallback(buttons_number);
+					#endif
 				}
 			} else {
-			    Buttons[buttons_number].Counter += timer_period_ms;
+			    Buttons[buttons_number]->Counter_Ms += timer_period_ms;
+				Buttons[buttons_number]->Counter_Ms++;
+				Buttons[buttons_number]->Counter_Ms = Buttons[buttons_number]->Counter_Ms + timer_period_ms;
 			}
 		} else {
-		    if (Buttons[buttons_number].Counter == 0) {
-			    if (Buttons[buttons_number].Condition_Current != RELEASED) {
-			        Buttons[buttons_number].Condition_Current = RELEASED;
+		    if (Buttons[buttons_number]->Counter_Ms == 0) {
+			    if (Buttons[buttons_number]->Condition_Current != RELEASED) {
+			        Buttons[buttons_number]->Condition_Current = RELEASED;
+					
+					#if  (BUTTON_CALLBACK == 1) 
+					ButtonsCallback(buttons_number);
+					#endif
 				}
 			} else {
-			    Buttons[buttons_number].Counter -= timer_period_ms;
+			    Buttons[buttons_number]->Counter_Ms -= timer_period_ms;
 			}
 		}
 	}
+	
 }
 
 
-BUTTON_STATE_T ButtonStateGet (ButtonsId id)
+BUTTON_CONDITION_T ButtonStateGet (BASE_T id)
 {
-    return Buttons[id].Condition_Current;
+    return Buttons[id]->Condition_Current;
 }
 
 
--- button.h
+++ button.h
@@ -1,16 +1,22 @@
+/*
+ ******************************************************************************
+ *
+ *
+ ******************************************************************************
+*/
 #ifndef  BUTTON_H
 #define  BUTTON_H
 
-#include  "types.h"
+
+#include  "uKernel/types.h"
 
 
 #define  BUTTON_LONG_PRESSED     0                            /* If to use should to set 1 */
 #define  BUTTON_CALLBACK         0
 //#define  USE_LONG_RELEASED    0                            /* If to use should to set 1 */   
 
-
-typedef  enun {
-    RELEASED,
+enum BUTTON_CONDITION {
+    RELEASED,  
 	PRESSED,
 #if  (BUTTON_LONG_PRESSED == 1)
     LONG_PRESSED,
@@ -18,20 +24,28 @@ typedef  enun {
 //#if  (USE_LONG_RELEASED == 1)
 //    LONG_RELEASED,
 //#endif
-} BUTTON_STATE_T;
+};
+
+typedef  enum BUTTON_CONDITION BUTTON_CONDITION_T;
 
 
 typedef  struct {
-    BASE_T Counter;
+    BASE_T Counter_Ms;
     BUTTON_CONDITION_T Condition_Current;
-    const BASE_T *Counter_Mark;    	
+    const BASE_T *Counter_Mark_Ms;    	
 #if  (BUTTON_USE_LONG_PRESSED == 1)
-    const BASE_T *Counter_Long_Mark;
+    const BASE_T *Counter_Long_Mark_Ms;
 #endif
     BUTTON_CONDITION_T Condition_Previous;
 	BOOL (* Pin_Chack) (void);
 	void (* Pin_Init) (void);
 } BUTTON_T;
+
+
+void ButtonsCreate (BASE_T id, const BASE_T *counter_mark_ms, const BASE_T *counter_long_mark_ms, BOOL (* pin_chack) (void), void (* pin_init) (void));
+void ButtonsInit (void);
+void ButtonsPolling (BASE_T timer_period_ms);
+BUTTON_CONDITION_T ButtonStateGet (BASE_T id);
 
 
 #endif  /* BUTTON_H */
